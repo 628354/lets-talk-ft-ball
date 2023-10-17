ce061e8d5efa3ae582510bfe4c6c39de35cf7b00
Developer <arvind@dightinfotech.com>
2023-10-17 15:35:12 +0530

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,53 +1,46 @@
+/** @format */
 
-import './App.css';
-import './css/arstyle.css';
-import './css/enstyle.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './page/Home';
-import About from './page/About';
-import Leagues from './page/Leagues';
-import Cafe from './page/Cafe';
-import Definition from './page/Definition';
-import Contact from './page/Contact';
-
-
-
-
-
+import "./App.css";
+import "./css/arstyle.css";
+import "./css/enstyle.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {  Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
+import About from "./page/About";
+import Leagues from "./page/Leagues";
+import Cafe from "./page/Cafe";
+import Definition from "./page/Definition";
+import Contact from "./page/Contact";
 
 function App() {
   return (
     <div className="App">
-  
-      
-      <Header/>
-     
-      <Routes>
-  <Route path="/" element={<Home />}> </Route>
-  <Route path="/About" element={<About />}> </Route>
-  <Route path="/Leagues" element={<Leagues />}> </Route>
-  <Route path="/Cafe" element={<Cafe />}> </Route>
-  <Route path="/Definition" element={<Definition />}> </Route>
-  <Route path="/Contact" element={<Contact />}> </Route>
+      <Header />
 
- 
-  
+        <Routes>
+          <Route path="/" element={<Home />}>
+            {" "}
+          </Route>
+          <Route path="/About" element={<About />}>
+            {" "}
+          </Route>
+          <Route path="/Leagues" element={<Leagues />}>
+            {" "}
+          </Route>
+          <Route path="/Cafe" element={<Cafe />}>
+            {" "}
+          </Route>
+          <Route path="/Definition" element={<Definition />}>
+            {" "}
+          </Route>
+          <Route path="/Contact" element={<Contact />}>
+            {" "}
+          </Route>
+        </Routes>
+    
 
-  
-  </Routes>
-  
-  
-
-
-
-
-
-      <Footer/>
-
-     
-      
+      <Footer />
     </div>
   );
 }
