18780a31a05417009f0f1b03269803970dfef087
Arvind <arvind@dightinfotech.com>
2023-11-25 14:13:36 +0000

    Teamdetailsl.js edited online with Bitbucket

--- src/Chart-filter-page/Teamdetailsl.js
+++ src/Chart-filter-page/Teamdetailsl.js
@@ -3,9 +3,87 @@ import { Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
 import Iframesecttion from '../Leagues-components/Iframesecttion';
-
-
+import { LineChart } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Line
+}
+const data = [
+  {
+    "name": "1",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "2",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "3",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "",
+    "uv": 2390,
+    "pv": 2800,
+    "amt": 2500
+  },
+  {
+    "name": "4",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  },
+  {
+    "name": "",
+    "uv": 3490,
+    "pv":2300,
+    "amt": 2100
+  },
+  {
+    "name": "5",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  },
+  {
+    "name": "",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  },
+  {
+    "name": "6",
+    "uv": 3490,
+    "pv": 9200,
+    "amt": 2100
+  }
+]
 export default function Teamdetailsl() {
+
   return (
     <div>
          <section className='en_hero_about en_hero_about'>
