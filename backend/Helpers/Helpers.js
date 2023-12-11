c6622a0e0a8f41918c61472210286b3a9f537903
DESKTOP-EDHUQ6L\Dell 006 <vipindightinfotech@gmail.com>
2023-12-11 14:55:51 +0530

    validation done

--- backend/Helpers/Helpers.js
+++ backend/Helpers/Helpers.js
@@ -37,11 +37,78 @@ const generateJWT = (email, firstname, lastname) => {
 
 }
 
+const checkValidation = async v => {
+    var errorsResponse;
 
+    await v.check().then(function (matched) {
+      if (!matched) {
+        var valdErrors = v.errors;
+        var respErrors = [];
+        Object.keys(valdErrors).forEach(function (key) {
+          if (valdErrors && valdErrors[key] && valdErrors[key].message) {
+            respErrors.push(valdErrors[key].message);
+          }
+        });
+        errorsResponse = respErrors.length > 0 ? respErrors[0] : '';
+      }
+    });
+    return errorsResponse;
+  }
+
+  const success =  (res, message = '', body = {}) => {
+    return res.status(200).json({
+      success: true,
+      code: 200,
+      message: message,
+      body: body,
+    });
+  }
+
+  const error = (res, err, req) => {
+    let code = typeof err === 'object' ? (err.code ? err.code : 403) : 403;
+    let message = typeof err === 'object'
+      ? err.message ? err.message : ''
+      : err;
+
+    if (req) {
+      req.flash('flashMessage', {
+        color: 'error',
+        message,
+      });
+
+      const originalUrl = req.originalUrl.split('/')[1];
+      return res.redirect(`/${originalUrl}`);
+    }
+
+    return res.status(code).json({
+      success: false,
+      message: message,
+      code: code,
+      body: {},
+    });
+  }
+
+ const failed = (res, message = '') => {
+    message = typeof message === 'object'
+      ? message.message ? message.message : ''
+      : message;
+      
+    return res.status(400).json({
+      success: false,
+      code: 400,
+      message: message,
+      body: {},
+    });
+  }
 module.exports = {
     encrypt,
     decrypt,
     getDateTime,
     removeLastCharacter,
-    generateJWT
+    generateJWT,
+    checkValidation,
+    success,
+    error,
+    failed
+
 }
