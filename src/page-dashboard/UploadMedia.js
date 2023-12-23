c040d1c3409739b95cbbf2510edce55f179bc540
crchandan <chandan@dightinfotech.com>
2023-12-23 18:52:26 +0530

    add page new

--- src/page-dashboard/UploadMedia.js
+++ src/page-dashboard/UploadMedia.js
@@ -22,6 +22,8 @@ function UploadMedia(props) {
     handleCloseModal();
   };
 
+  
+
 
 
     return (
@@ -56,7 +58,7 @@ function UploadMedia(props) {
                     <li className='add-button-fis'>
                       <Link to="" onClick={handleShowModal}><i className="ri-add-line"></i></Link>
                     </li>
-                    <li class="add-button-cencel"><a href=""><i className="ri-refresh-line"></i></a></li>
+                    <li className="add-button-cencel"><a href=""><i className="ri-refresh-line"></i></a></li>
                     {/* <li className='add-button-sec'>
                       <button><i className="ri-delete-bin-line"></i></button>
                     </li> */}
@@ -76,12 +78,263 @@ function UploadMedia(props) {
                         <Row>
                         <div className='main_add_season'>
                         <div className='main-ad-sea'>
-                        <p><i class="ri-pencil-fill"></i>Add Folder</p>
+                        <p><i className="ri-pencil-fill"></i>Add Folder</p>
                         </div>
                         </div>
                         </Row>
                     </Container>
                 </section>
+
+                <section className='media-section'>
+                <Container fluid >
+                  <Row>
+                  <div className='col-lg-2 col-sm-2 col-sm-2 pe-0'>
+                   <div className='main-media-ponier'>
+                     <div className='media-ponite-part'>
+                     <div className="px-3 pt-2 styt">
+                
+                <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start npmless" id="menu">
+                    
+                    <li>
+                        <a href="#media1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                        <span className='cat-icon'><i className="ri-arrow-down-s-fill"></i></span> <span className='folder-main'><i className="ri-folder-fill"></i></span> <span className="ms-1 d-none d-sm-inline less_cat">catalog</span> </a>
+                        <ul className="collapse show nav flex-column ms-1 npmless left-pad" id="media1" data-bs-parent="#menu">
+                            <li className="w-100">
+                            <a href="#media2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
+                            <span className='cat-icon'><i class="ri-arrow-down-s-fill"></i></span>
+                            <i className="fs-4 bi-bootstrap"></i>  <span className='folder-main'><i className="ri-folder-fill"></i></span> <span className="ms-1 d-none d-sm-inline less_cat">advertikon</span></a>
+                              <ul className="collapse nav flex-column ms-1" id="media2" data-bs-parent="#menu">
+                                  <li className="w-100">
+                                      <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline less_cat">adk_gdpr</span></a>
+                                  </li>
+                              </ul>
+                            </li>
+
+                            <li>
+                              <a href="#media3" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
+                              <span className='cat-icon'><i className="ri-arrow-down-s-fill"></i></span>
+                              <span className='folder-main'><i className="ri-folder-fill"></i></span> <span className="ms-1 d-none d-sm-inline less_cat">Articles</span></a>
+                              <ul className="collapse nav flex-column ms-1" id="media3" data-bs-parent="#menu">
+                               <li className="w-100">
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline less_cat">PL</span></a>
+                              </li>
+                              <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline less_cat">S Pro</span></a>
+                            </li>
+                          </ul>
+                         </li>
+                         <li>
+                           <a href="#" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-people"></i> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline less_cat">blog</span> </a>
+                       </li>
+                       <li>
+                           <a href="#" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-people"></i> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline less_cat">ckeditor</span> </a>
+                       </li>
+                       <li>
+                           <a href="#" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-people"></i> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline less_cat">d_gdpr_module</span> </a>
+                       </li>
+                       <li>
+                           <a href="#" className="nav-link px-0 align-middle">
+                             <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline less_cat">demo</span> </a>
+                       </li>
+                       <li>
+                           <a href="#" className="nav-link px-0 align-middle">
+                           <span className='folder-main'><i className="ri-folder-fill"></i></span> <span class="ms-1 d-none d-sm-inline less_cat">leagues</span> </a>
+                       </li>
+
+                       <li>
+                        <a href="#media4" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
+                        <span className='cat-icon'><i className="ri-arrow-down-s-fill"></i></span>
+                        <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline less_cat">log</span></a>
+                        <ul className="collapse nav flex-column ms-1 npmless" id="media4" data-bs-parent="#menu">
+                            <li className="w-100">
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Austria</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Belgum</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Botola</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Boundeslega</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Braz</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Croatia</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Czech</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Denmark</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Egy Pro</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">India</span></a>
+                            </li><li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Iraq</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Israel</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Laliga logos</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Legu 1</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Netherland</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Portugal</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Premier L</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Portugal</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Portugal</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Portugal</span></a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Portugal</span></a>
+                            </li>
+                          
+
+
+                        </ul>
+                    </li>
+                       <li>
+                            <a href="#media5" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
+                            <span className='cat-icon'><i className="ri-arrow-down-s-fill"></i></span>
+                            <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline">ocdevwizard</span></a>
+                          <ul className="collapse nav flex-column ms-1 npmless" id="media5" data-bs-parent="#menu">
+                            <li className="w-100">
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Item</span></a>
+                            </li>
+                          <li>
+                                <a href="#" className="nav-link px-0"> <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="d-none d-sm-inline">Item</span></a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                        <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline">page</span> </a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                             <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline">slider</span> </a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                             <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline">team</span> </a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                             <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline">thumbnail</span> </a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                             <span className='folder-main'><i className="ri-folder-fill"></i></span>  <span className="ms-1 d-none d-sm-inline">WORLDCUP</span> </a>
+                    </li>
+                    
+
+
+                        </ul>
+                    </li>
+                    
+                    
+                   
+                </ul>
+                
+            </div>
+                     </div>
+                   </div>
+                  </div>
+
+                  <div className='col-lg-10 col-sm-10 col-sm-10 ps-0'>
+                   <div className="up-date-file">
+                        <Row>
+                        <div className="col-lg-2 col-md-2 col-sm-6">
+                         <div className='main-media-folder'>
+                         <div class="file-wrapper">
+                          <input type="file" name="upload-img" accept="image/*" />
+                          <div class="close-btn">×</div>
+                        </div>
+                      </div>
+                     </div>
+
+                     <div className="col-lg-2 col-md-2 col-sm-6">
+                         <div className='media-folder-image'>
+                         <img
+                          src={require("../img/Banner-559x363.png")}
+                          alt="earth"
+                          className="up-date-img"
+                        />
+                        
+                      </div>
+                     </div>
+                     <div className="col-lg-2 col-md-2 col-sm-6">
+                         <div className='media-folder-image'>
+                         <img
+                          src={require("../img/_(2)-559x363.png")}
+                          alt="earth"
+                          className="up-date-img"
+                        />
+                        
+                      </div>
+                     </div>
+                     <div className="col-lg-2 col-md-2 col-sm-6">
+                         <div className='media-folder-image'>
+                         <img
+                          src={require("../img/_arsenal-559x363.jpg")}
+                          alt="earth"
+                          className="up-date-img"
+                        />
+                        
+                      </div>
+                     </div>
+                     <div className="col-lg-2 col-md-2 col-sm-6">
+                         <div className='media-folder-image'>
+                         <img
+                          src={require("../img/bigest-transfers-559x363.png")}
+                          alt="earth"
+                          className="up-date-img"
+                        />
+                        
+                      </div>
+                     </div>
+                     <div className="col-lg-2 col-md-2 col-sm-6">
+                         <div className='media-folder-image'>
+                         <img
+                          src={require("../img/ars 1-559x363.png")}
+                          alt="earth"
+                          className="up-date-img"
+                        />
+                        
+                      </div>
+                     </div>
+                        </Row>
+
+
+                   </div>
+                  </div>
+                  
+                  </Row>
+                </Container>
+              </section>
             </div>
 {/*  */}
 <Modal show={showModal} onHide={handleCloseModal}>
@@ -150,6 +403,13 @@ function UploadMedia(props) {
 </div>
 </div>
 </div> */}
+
+
+           
+
+
+
+
         </div>
     );
 }
